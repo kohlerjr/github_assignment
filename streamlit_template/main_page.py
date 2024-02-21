28b7e6cd33ecb512a96a3f7f06f8c30fdf490f50
Jacob Kohler <15050754+kohlerjr@users.noreply.github.com>
2024-02-20 22:05:59 -0700

    Adding navigation links

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -6,3 +6,6 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer stats")
 
+link = '[To my Github Pages Site](https://kohlerjr.github.io/github_assignment/)'
+st.markdown(link, unsafe_allow_html=True)
+
